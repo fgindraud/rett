a3cd3a6e2ed8eed5311eb86ff50dbccef2eb7cfb
François Gindraud <francois.gindraud@gmail.com>
2019-07-15 20:59:39 +0200

    Removed separate utils file

--- src/relations.rs
+++ src/relations.rs
@@ -4,7 +4,6 @@ use std::fmt;
 use std::hash::Hash;
 use std::io;
 use std::marker::PhantomData;
-use utils::Set;
 
 /// Error type for graph operations
 #[derive(Debug, Eq, PartialEq)]
@@ -395,6 +394,36 @@ impl<T> std::ops::Index<usize> for SlotVec<T> {
 impl<T> std::ops::IndexMut<usize> for SlotVec<T> {
     fn index_mut(&mut self, i: usize) -> &mut Self::Output {
         self.get_mut(i).unwrap()
+    }
+}
+
+/// Set based on a sorted vector.
+#[derive(Debug, PartialEq, Eq)]
+pub struct Set<T: Ord> {
+    inner: Vec<T>,
+}
+impl<T: Ord> Set<T> {
+    pub fn new() -> Self {
+        Set { inner: Vec::new() }
+    }
+    pub fn contains(&self, e: &T) -> bool {
+        self.inner.binary_search(e).is_ok()
+    }
+    pub fn insert(&mut self, e: T) {
+        if let Err(insertion_index) = self.inner.binary_search(&e) {
+            self.inner.insert(insertion_index, e)
+        }
+    }
+    pub fn remove(&mut self, e: &T) {
+        if let Ok(index) = self.inner.binary_search(e) {
+            self.inner.remove(index);
+        }
+    }
+}
+impl<T: Ord> std::ops::Deref for Set<T> {
+    type Target = [T];
+    fn deref(&self) -> &[T] {
+        self.inner.deref()
     }
 }
 
